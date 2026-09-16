a40d158ac3c2f103a458e0da49512dcd74dcb066
ziamudassir041-cell <ziamudassir041@gmail.com>
2026-09-16 15:42:01 +0000

    manager onboarding completed

--- version1/src/app/manager/onboarding/layout.tsx
+++ version1/src/app/manager/onboarding/layout.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-import { ManagerOnboardingProvider } from "@/components/manager/manager-onboarding-provider";
+import { ManagerOnboardingProvider } from "../../../components/manager/manager-onboarding-provider";
 
 export default function ManagerOnboardingLayout({ children }: { children: ReactNode }) {
   return <ManagerOnboardingProvider>{children}</ManagerOnboardingProvider>;
